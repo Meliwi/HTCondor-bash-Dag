739e92b90def9775280ed83d65e857d6215becba
meliwi <melissa.gonzalez@correounivalle.edu.co>
2022-08-01 14:26:59 -0500

    added txt

--- node2.sh
+++ node2.sh
@@ -7,7 +7,7 @@ fi
 data=$1
 total_reg=$(wc -l $data)
 incomp_reg=$(grep -c ", ?," $data)
-delete_incomp_reg=$(sed -i "s/, ?,/,,/g" datos-adultos-us.csv)
+delete_incomp_reg=$(sed -i "s/, ?,/,,/g" $data)
 
 echo $total_reg
 echo $incomp_reg
