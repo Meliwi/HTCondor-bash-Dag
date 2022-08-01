47db40c3076ce48334e5b0fd6b6bca136297d8dc
meliwi <melissa.gonzalez@correounivalle.edu.co>
2022-08-01 15:07:28 -0500

    added to node5.sh

--- node5.sh
+++ node5.sh
@@ -8,5 +8,5 @@ data=$1
 people_black_f=$(grep "Black"  $data | grep ">50K" | wc -l)
 people_female_f=$(grep "Female" $data | grep ">50K" |  wc -l)
 
-echo ${people_black_f}
-echo ${people_female_f}
+echo "N° Black people with income >50K: ${people_black_f}"
+echo "N° Females with income >50K: ${people_female_f}"
